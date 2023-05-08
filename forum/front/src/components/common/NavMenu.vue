88f9945fae3674766326dc2f67b9fbb6d90adb7a
863158782 <863158782@qq.com>
2023-05-08 19:44:52 +0800

    five commit

---
+++ forum/front/src/components/common/NavMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-menu
+    :default-active="'/index'"
+    router
+    mode="horizontal"
+    background-color="white"
+    text-color="#222"
+    active-text-color="red"
+    style="min-width: 1300px">
+    <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
+      {{ item.navItem }}
+    </el-menu-item>
+    <el-button @click="logout()" style="position: absolute; top: 10px;right: 100px;">注销</el-button>
+    <span style="position: absolute;padding-top: 20px;right: 50%;font-size: 20px;font-weight: bold">Library</span>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'NavMenu',
+  data () {
+    return {
+      navList: [
+        {name: '/index', navItem: '首页'},
+        {name: '/jotter', navItem: '笔记本'},
+        {name: '/library', navItem: '图书馆'},
+        {name: '/admin', navItem: '个人中心'}
+      ]
+    }
+  },
+  methods:{
+    logout(){
+      window.localStorage.removeItem('user')
+      window.location.href='/login';
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+a{
+  text-decoration: none;
+}
+
+span {
+  pointer-events: none;
+}
+</style>
