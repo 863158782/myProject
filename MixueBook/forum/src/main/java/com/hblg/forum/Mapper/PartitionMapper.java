9921cadf65b67f9ac5d21eb05a88d7914a89510a
863158782 <72909286+863158782@users.noreply.github.com>
2023-05-23 09:05:01 +0800

    Delete MixueBook/forum directory

--- MixueBook/forum/src/main/java/com/hblg/forum/Mapper/PartitionMapper.java
+++
@@ -1,7 +0,0 @@
-package com.hblg.forum.Mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.hblg.forum.Entity.Partition;
-
-public interface PartitionMapper extends BaseMapper<Partition> {
-}
--- MixueBook/forum/src/main/java/com/hblg/forum/Mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hblg.forum.Mapper;
-
-import com.hblg.forum.Entity.User;
-import org.apache.ibatis.annotations.Mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User>{
-}
